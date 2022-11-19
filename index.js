ef76eb742476ad9792845db09987b1aac126f0f8
Rankrish <59313760+purusr@users.noreply.github.com>
2022-11-19 20:24:28 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import Student from './studentmodel.js';
+
+mongoose.connect('mongodb+srv://krish-node:Krishna8686@cluster0.emvic.mongodb.net/?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=> console.log('DB connected'))
+.catch((error)=> console.log('DB has connection error'));
+
+const app = express();
+
+
+app.get('/', (req,res)=>{
+  res.send('Server is live')
+})
+
+
+app.get('/createstudent', async(req,res)=>{
+  
+
+    await new Student({roll:1, name: 'kunal', department: 'BCA',contact: 1234,identity:'M'})
+
+})
+
+
+app.get('/studentdata', async(req,res)=>{
+    const user = await Student.find()
+
+    res.json(user);
+})
+app.listen(5000,()=>{
+    console.log('Running on port 5000')
+})
